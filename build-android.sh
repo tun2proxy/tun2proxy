b8c22db037af267f498d80d945a3a0326ae8e291
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-06-18 17:44:06 +0800

    build-android.sh

--- build-android.sh
+++ build-android.sh
@@ -2,13 +2,18 @@
 
 work_dir=$(pwd)
 
+ANDROID_API_VERSION=21
+# NDK homepage: https://developer.android.com/ndk/downloads#lts-downloads
+ANDROID_NDK_VERSION=26.3.11579264
+# Android commandline tools homepage: https://developer.android.com/studio/index.html#command-line-tools-only
+CMDLINE_TOOLS_VERSION=6858069
+
 export ANDROID_HOME=/tmp/Android/sdk
-export NDK_HOME=${ANDROID_HOME}/ndk/25.2.9519653
+export NDK_HOME=${ANDROID_HOME}/ndk/${ANDROID_NDK_VERSION}
 export PATH=$ANDROID_HOME/cmdline-tools/bin:$PATH
 mkdir -p $ANDROID_HOME
 
 name=tun2proxy
-package=tun2proxy
 BASE=`dirname "$0"`
 android_libs=$BASE/${name}-android-libs
 mkdir -p $android_libs
@@ -17,13 +22,21 @@ function setup_env() {
     cargo install cbindgen
     apt update && apt install -y make llvm-dev libclang-dev clang pkg-config zip unzip curl default-jdk build-essential
     cd /tmp/
-    curl -OL https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip
+    curl -OL https://dl.google.com/android/repository/commandlinetools-linux-${CMDLINE_TOOLS_VERSION}_latest.zip
     rm -rf /tmp/cmdline-tools
-    unzip commandlinetools-linux-6858069_latest.zip
+    unzip commandlinetools-linux-${CMDLINE_TOOLS_VERSION}_latest.zip
     rm -rf $ANDROID_HOME/cmdline-tools
     mv cmdline-tools $ANDROID_HOME
     yes | sdkmanager --sdk_root=$ANDROID_HOME --licenses
-    sdkmanager --sdk_root=$ANDROID_HOME "ndk;25.2.9519653" "platforms;android-21"
+    if [ $? -ne 0 ]; then
+        echo "Failed to accept the licenses"
+        exit 1
+    fi
+    sdkmanager --sdk_root=$ANDROID_HOME "ndk;${ANDROID_NDK_VERSION}" "platforms;android-${ANDROID_API_VERSION}"
+    if [ $? -ne 0 ]; then
+        echo "Failed to install NDK"
+        exit 1
+    fi
 }
 
 function build_android() {
@@ -50,37 +63,35 @@ function build_android() {
     local BASE=`dirname "$0"`
     local HOST_OS=`uname -s | tr "[:upper:]" "[:lower:]"`
     local HOST_ARCH=`uname -m | tr "[:upper:]" "[:lower:]"`
-
-    export PATH="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin/":$PATH
-
     local android_tools="$NDK_HOME/toolchains/llvm/prebuilt/$HOST_OS-$HOST_ARCH/bin"
-    local api=21
+
+    export PATH="${android_tools}/":$PATH
 
     for target in $targets; do
         local target_dir=
         case $target in
             'armv7-linux-androideabi')
-                export CC_armv7_linux_androideabi="$android_tools/armv7a-linux-androideabi${api}-clang"
+                export CC_armv7_linux_androideabi="$android_tools/armv7a-linux-androideabi${ANDROID_API_VERSION}-clang"
                 export AR_armv7_linux_androideabi="$android_tools/llvm-ar"
-                export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="$android_tools/armv7a-linux-androideabi${api}-clang"
+                export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER="$android_tools/armv7a-linux-androideabi${ANDROID_API_VERSION}-clang"
                 target_dir=armeabi-v7a
                 ;;
             'x86_64-linux-android')
-                export CC_x86_64_linux_android="$android_tools/${target}${api}-clang"
+                export CC_x86_64_linux_android="$android_tools/${target}${ANDROID_API_VERSION}-clang"
                 export AR_x86_64_linux_android="$android_tools/llvm-ar"
-                export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
+                export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$android_tools/${target}${ANDROID_API_VERSION}-clang"
                 target_dir=x86_64
                 ;;
             'aarch64-linux-android')
-                export CC_aarch64_linux_android="$android_tools/${target}${api}-clang"
+                export CC_aarch64_linux_android="$android_tools/${target}${ANDROID_API_VERSION}-clang"
                 export AR_aarch64_linux_android="$android_tools/llvm-ar"
-                export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
+                export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$android_tools/${target}${ANDROID_API_VERSION}-clang"
                 target_dir=arm64-v8a
                 ;;
             'i686-linux-android')
-                export CC_i686_linux_android="$android_tools/${target}${api}-clang"
+                export CC_i686_linux_android="$android_tools/${target}${ANDROID_API_VERSION}-clang"
                 export AR_i686_linux_android="$android_tools/llvm-ar"
-                export CARGO_TARGET_I686_LINUX_ANDROID_LINKER="$android_tools/${target}${api}-clang"
+                export CARGO_TARGET_I686_LINUX_ANDROID_LINKER="$android_tools/${target}${ANDROID_API_VERSION}-clang"
                 target_dir=x86
                 ;;
             *)
@@ -88,8 +99,13 @@ function build_android() {
                 ;;
         esac
         cargo build --target $target $mode
+        if [ $? -ne 0 ]; then
+            echo "Failed to build for target $target"
+            exit 1
+        fi
         mkdir -p $android_libs/$target_dir
         cp $BASE/target/$target/${mode2}/lib${name}.so $android_libs/${target_dir}/lib${name}.so
+        cp $BASE/target/$target/${mode2}/lib${name}.a $android_libs/${target_dir}/lib${name}.a
     done
 
     cbindgen -c $BASE/cbindgen.toml -l C --cpp-compat -o $android_libs/$name.h
